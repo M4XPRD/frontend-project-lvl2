6b1b82d17a1392ecee0b3bc843dc2e07874395cf
M4X <maxprd@M4X.localdomain>
2021-12-14 05:31:17 +0300

    Added bin and type module for gendiff

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,10 +1,11 @@
 #!/usr/bin/env node
 
-import { Command } from 'commander';
+import { Command } from 'commander/esm.mjs';
 
 const program = new Command();
+
 program
   .description('Compares two configuration files and shows a difference.')
-  .version('0.0.3', '-V, --version', 'output the version number');
+  .version('0.0.11111111111', '-V, --version', 'output the version number');
 
 program.parse();
