2d31b270ee2fdb6740b463e5e5ce2afbb9a7b95c
M4X <maxprd@M4X.localdomain>
2021-12-20 18:12:34 +0300

    Step 2 done

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -6,6 +6,8 @@ const program = new Command();
 
 program
   .description('Compares two configuration files and shows a difference.')
-  .version('1.0', '-V, --version', 'output the version number');
+  .version('0.0.1', '-V, --version', 'output the version number')
+  .option('-f, --format [type]', 'output format')
+  .argument('<filepath1> <filepath2>');
 
 program.parse();
