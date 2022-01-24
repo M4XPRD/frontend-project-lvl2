23e4f934f57bf24d2f8d86836c43907fab0ad6f8
Max <ppmax@bk.ru>
2022-01-24 21:07:06 +0300

    Installed lodash

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,29 +1,19 @@
+/* eslint-disable indent */
+/* eslint-disable no-undef */
 module.exports = {
-    'env': {
-        'browser': true,
-        'es2021': true
-    },
-    'extends': 'eslint:recommended',
-    'parserOptions': {
-        'ecmaVersion': 'latest',
-        'sourceType': 'module'
-    },
-    'rules': {
-        'indent': [
-            'error',
-            4
-        ],
-        'linebreak-style': [
-            'error',
-            'windows'
-        ],
-        'quotes': [
-            'error',
-            'single'
-        ],
-        'semi': [
-            'error',
-            'always'
-        ]
-    }
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: 'eslint:recommended',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    indent: ['error', 4],
+    'linebreak-style': ['error', 'windows'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'always'],
+  },
 };
