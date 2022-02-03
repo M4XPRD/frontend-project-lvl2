78c6df38a5e8a8e8ea48cce0c4c76a6d946efb05
Max <ppmax@bk.ru>
2022-02-04 00:27:46 +0300

    Added code to genDiff

---
+++ src/genDiff.js
@@ -0,0 +1,24 @@
+import _ from 'lodash';
+
+const genDiff = (data1, data2) => {
+  const keys1 = _.keys(data1);
+  const keys2 = _.keys(data2);
+  const keys = _.union(keys1, keys2);
+
+  const result = {};
+  for (const key of keys) {
+    if (!_.has(data1, key)) {
+      result[key] = 'added';
+    } else if (!_.has(data2, key)) {
+      result[key] = 'deleted';
+    } else if (data1[key] !== data2[key]) {
+      result[key] = 'changed';
+    } else {
+      result[key] = 'unchanged';
+    }
+  }
+
+  return result;
+};
+
+export default genDiff;
