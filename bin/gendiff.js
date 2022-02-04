a446e34f5c61e5fec0ff9c5926192951ec2145f5
Max <ppmax@bk.ru>
2022-02-05 02:12:58 +0300

    Fixed some code

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -5,9 +5,9 @@ import { Command } from 'commander/esm.mjs';
 const program = new Command();
 
 program
-    .description('Compares two configuration files and shows a difference.')
-    .version('0.0.1', '-V, --version', 'output the version number')
-    .option('-f, --format [type]', 'output format')
-    .argument('<filepath1> <filepath2>');
+  .description('Compares two configuration files and shows a difference.')
+  .version('0.0.1', '-V, --version', 'output the version number')
+  .option('-f, --format [type]', 'output format')
+  .argument('<filepath1> <filepath2>');
 
 program.parse();
