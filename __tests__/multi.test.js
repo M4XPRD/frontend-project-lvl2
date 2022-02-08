a6164883ab80e34848dd705e3a66f2637e2d72c0
Max <ppmax@bk.ru>
2022-02-08 12:12:45 +0300

    Small fixes

--- __tests__/multi.test.js
+++ __tests__/multi.test.js
@@ -2,5 +2,4 @@ import multi from '../src/multi.js';
 
 test('multi', () => {
   expect(multi(1, 2)).toEqual(2);
-  expect(multi(0, 5)).toEqual(0);
 });
--- __tests__/sum.test.js
+++ __tests__/sum.test.js
@@ -2,5 +2,4 @@ import sum from '../src/sum.js';
 
 test('sum', () => {
   expect(sum(1, 2)).toEqual(3);
-  expect(sum(0, 5)).toEqual(5);
 });
