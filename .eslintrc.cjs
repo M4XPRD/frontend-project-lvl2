1c29c094c0aeccae5229dabaa858906f0173cceb
Max <ppmax@bk.ru>
2022-02-08 11:34:22 +0300

    Fixed eslint #3

---
+++ .eslintrc.cjs
@@ -0,0 +1,18 @@
+/* eslint-disable no-undef */
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: 'eslint:recommended',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    indent: ['error', 2],
+    'linebreak-style': ['error', 'windows'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'always'],
+  },
+};
