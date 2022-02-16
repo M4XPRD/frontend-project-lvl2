8ee9ccb76bb224b53098926d7b7b7f154ffb2992
Max <ppmax@bk.ru>
2022-02-17 02:30:18 +0300

    gendiff fix

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,13 +1,15 @@
 #!/usr/bin/env node
 
-import { Command } from 'commander/esm.mjs';
-
-const program = new Command();
+import { program } from 'commander';
+import genDiff from '../src/genDiff.js';
 
 program
   .description('Compares two configuration files and shows a difference.')
   .version('0.0.1', '-V, --version', 'output the version number')
-  .option('-f, --format [type]', 'output format')
-  .argument('<filepath1> <filepath2>');
+  .option('-f, --format <type>', 'output format')
+  .argument('<filepath1> <filepath2>')
+  .action((filepath1, filepath2) => {
+    console.log(genDiff(filepath1, filepath2));
+  });
 
 program.parse();
