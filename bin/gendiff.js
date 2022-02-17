e4f035475dc470fe7a53620731c83b81245263e6
Max <ppmax@bk.ru>
2022-02-18 00:54:01 +0300

    Fixed arguements with adding 's' to the end

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -7,7 +7,7 @@ program
   .description('Compares two configuration files and shows a difference.')
   .version('0.0.1', '-V, --version', 'output the version number')
   .option('-f, --format <type>', 'output format')
-  .argument('<filepath1> <filepath2>')
+  .arguments('<filepath1> <filepath2>')
   .action((filepath1, filepath2) => {
     console.log(genDiff(filepath1, filepath2));
   });
