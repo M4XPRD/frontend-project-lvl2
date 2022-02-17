342e6346349fde3f920a3c89c7f806d7ed7e7258
Max <ppmax@bk.ru>
2022-02-18 00:53:26 +0300

    Created genDiff file

--- src/genDiff.js
+++ src/genDiff.js
@@ -1,28 +1,27 @@
-/* eslint-disable no-restricted-syntax */
 import _ from 'lodash';
+import parseFile from './parsing.js';
 
-const genDiff = (data1, data2) => {
-  const json1 = JSON.parse(data1);
-  const json2 = JSON.parse(data2);
-  const keys1 = _.keys(json1);
-  const keys2 = _.keys(json2);
+const genDiff = (filepath1, filepath2) => {
+  const file1 = parseFile(filepath1);
+  const file2 = parseFile(filepath2);
+  const keys1 = _.keys(file1);
+  const keys2 = _.keys(file2);
   const keys = _.sortBy(_.union(keys1, keys2));
 
-  const result = [];
-  for (const key of keys) {
-    if (!_.has(json1, key)) {
-      result.push(`+ ${key}: ${json2[key]}`);
-    } else if (!_.has(json2, key)) {
-      result.push(`- ${key}: ${json1[key]}`);
-    } else if (json1[key] !== json2[key]) {
-      result.push(`- ${key}: ${json1[key]}`, `+ ${key}: ${json2[key]}`);
-    } else if (json1[key] === json2[key]) {
-      result.push(`  ${key}: ${json1[key]}`);
+  const result = keys.reduce((array, key) => {
+    if (!_.has(file1, key)) {
+      array.push(`+ ${key}: ${file2[key]}`);
+    } else if (!_.has(file2, key)) {
+      array.push(`- ${key}: ${file1[key]}`);
+    } else if (file1[key] !== file2[key]) {
+      array.push(`- ${key}: ${file1[key]}`, `+ ${key}: ${file2[key]}`);
+    } else if (file1[key] === file2[key]) {
+      array.push(`  ${key}: ${file1[key]}`);
     }
-  }
+    return array;
+  }, []);
+
   return ['{', ...result, '}'].join('\n');
 };
 
 export default genDiff;
-
-console.log(genDiff('{ "host": "hexlet.io", "timeout": 50, "proxy": "123.234.53.22", "follow": false }', '{ "timeout": 20, "verbose": true, "host": "hexlet.io" }'));
