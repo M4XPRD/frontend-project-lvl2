6c3b345b3ea2dd68f6f9e5eb8629c6977346bce5
Max <ppmax@bk.ru>
2022-02-18 18:22:26 +0300

    Test for 2 JSON's #1

--- __tests__/genDiff.test.js
+++ __tests__/genDiff.test.js
@@ -1,1 +1,15 @@
-// import genDiff from '../src/genDiff';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import genDiff from '../src/genDiff';
+import expectedJson from '../__fixtures__/expectedJson';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+
+test('difference between two JSONs', () => {
+  const filepath1 = getFixturePath('../__fixtures__/file1.json');
+  const filepath2 = getFixturePath('../__fixtures__/file2.json');
+
+  expect(genDiff(filepath1, filepath2)).toBe(expectedJson);
+});
