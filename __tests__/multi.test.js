118773af99c641d48b9f4eff34b624bc409e9757
Max <ppmax@bk.ru>
2022-02-18 18:21:34 +0300

    Deleted the decoy tests

--- __tests__/multi.test.js
+++
@@ -1,5 +0,0 @@
-import multi from '../src/multi.js';
-
-test('multi', () => {
-  expect(multi(1, 2)).toEqual(2);
-});
--- __tests__/sum.test.js
+++
@@ -1,5 +0,0 @@
-import sum from '../src/sum.js';
-
-test('sum', () => {
-  expect(sum(1, 2)).toEqual(3);
-});
