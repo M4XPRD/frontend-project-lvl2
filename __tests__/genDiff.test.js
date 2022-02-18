300bcf2a88b272cfbc8eecd0a0692ab6a91ee583
Max <ppmax@bk.ru>
2022-02-19 02:20:22 +0300

    Created some new tests

--- __tests__/genDiff.test.js
+++ __tests__/genDiff.test.js
@@ -1,15 +1,34 @@
 import path from 'path';
 import { fileURLToPath } from 'url';
 import genDiff from '../src/genDiff';
-import expectedJson from '../__fixtures__/expectedJson';
+import expectedResult from '../__fixtures__/expectedResult';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 
 test('difference between two JSONs', () => {
-  const filepath1 = getFixturePath('../__fixtures__/file1.json');
-  const filepath2 = getFixturePath('../__fixtures__/file2.json');
+  const filepath1 = getFixturePath('file1.json');
+  const filepath2 = getFixturePath('file2.json');
 
-  expect(genDiff(filepath1, filepath2)).toBe(expectedJson);
+  expect(genDiff(filepath1, filepath2)).toBe(expectedResult);
+});
+
+test('difference between two YAMLs', () => {
+  const filepath1 = getFixturePath('file1.yml');
+  const filepath2 = getFixturePath('file2.yml');
+
+  expect(genDiff(filepath1, filepath2)).toBe(expectedResult);
+});
+
+test('difference between JSON and YAML', () => {
+  const filepath1 = getFixturePath('file1.json');
+  const filepath2 = getFixturePath('file2.yml');
+
+  expect(genDiff(filepath1, filepath2)).toBe(expectedResult);
+
+  const filepath3 = getFixturePath('file1.yml');
+  const filepath4 = getFixturePath('file2.json');
+
+  expect(genDiff(filepath3, filepath4)).toBe(expectedResult);
 });
