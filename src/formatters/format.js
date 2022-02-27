da20072bf253360e4f476c74b32b953ccc69b7f6
Max <ppmax@bk.ru>
2022-02-27 17:14:39 +0300

    Small fix to formatter

--- src/formatters/format.js
+++ src/formatters/format.js
@@ -1,7 +1,7 @@
 import stylish from './stylish.js';
 import json from './json.js';
 
-const format = (file, formatName) => {
+const format = (file, formatName = 'stylish') => {
   switch (formatName) {
     case 'stylish':
       return stylish(file);
