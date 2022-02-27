10220b02faf3acec2caf841389de66e08f49b34e
Max <ppmax@bk.ru>
2022-02-27 16:56:44 +0300

    Test fixed

--- __tests__/genDiff.test.js
+++ __tests__/genDiff.test.js
@@ -1,34 +1,28 @@
 import path from 'path';
 import { fileURLToPath } from 'url';
 import genDiff from '../src/genDiff';
-import expectedResult from '../__fixtures__/expectedResult';
+import expected from '../__fixtures__/expected-result';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+const getFixturePath = (filename) => {
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+  return path.join(__dirname, '..', '__fixtures__', filename);
+};
+
+const filepath1 = getFixturePath('file1.json');
+const filepath2 = getFixturePath('file2.json');
+const filepath3 = getFixturePath('file1.yml');
+const filepath4 = getFixturePath('file2.yaml');
 
 test('difference between two JSONs', () => {
-  const filepath1 = getFixturePath('file1.json');
-  const filepath2 = getFixturePath('file2.json');
-
-  expect(genDiff(filepath1, filepath2)).toBe(expectedResult);
+  expect(genDiff(filepath1, filepath2)).toBe(expected);
 });
 
 test('difference between two YAMLs', () => {
-  const filepath1 = getFixturePath('file1.yml');
-  const filepath2 = getFixturePath('file2.yml');
-
-  expect(genDiff(filepath1, filepath2)).toBe(expectedResult);
+  expect(genDiff(filepath1, filepath2)).toBe(expected);
 });
 
 test('difference between JSON and YAML', () => {
-  const filepath1 = getFixturePath('file1.json');
-  const filepath2 = getFixturePath('file2.yml');
-
-  expect(genDiff(filepath1, filepath2)).toBe(expectedResult);
-
-  const filepath3 = getFixturePath('file1.yml');
-  const filepath4 = getFixturePath('file2.json');
-
-  expect(genDiff(filepath3, filepath4)).toBe(expectedResult);
+  expect(genDiff(filepath1, filepath2)).toBe(expected);
+  expect(genDiff(filepath3, filepath4)).toBe(expected);
 });
