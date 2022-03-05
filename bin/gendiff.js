2321c1eca51f319c4876f638160b90d1daea4416
Max <ppmax@bk.ru>
2022-03-05 05:38:48 +0300

    Updated format

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,14 +1,14 @@
 #!/usr/bin/env node
 
 import { program } from 'commander';
-import genDiff from '../src/genDiff.js';
+import genDiff from '../src/index.js';
 
 program
   .description('Compares two configuration files and shows a difference.')
   .version('0.0.1', '-V, --version', 'output the version number')
   .option('-f, --format <type>', 'output format', 'stylish')
   .arguments('<filepath1> <filepath2>')
-  .action((filepath1, filepath2, format) => {
+  .action((filepath1, filepath2, { format }) => {
     const diff = genDiff(filepath1, filepath2, format);
     console.log(diff);
   });
