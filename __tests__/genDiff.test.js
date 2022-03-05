5f1ebde1c4fd99713041142e6d686b9a2572e7eb
Max <ppmax@bk.ru>
2022-03-05 05:17:42 +0300

    Tests updated

--- __tests__/genDiff.test.js
+++ __tests__/genDiff.test.js
@@ -1,7 +1,7 @@
 import path from 'path';
+import { readFileSync } from 'fs';
 import { fileURLToPath } from 'url';
-import genDiff from '../src/genDiff';
-import expected from '../__fixtures__/expected-result';
+import genDiff from '../src/index.js';
 
 const getFixturePath = (filename) => {
   const __filename = fileURLToPath(import.meta.url);
@@ -9,20 +9,18 @@ const getFixturePath = (filename) => {
   return path.join(__dirname, '..', '__fixtures__', filename);
 };
 
+const readFile = (filename) => readFileSync(getFixturePath(filename), 'utf-8');
+
 const filepath1 = getFixturePath('file1.json');
 const filepath2 = getFixturePath('file2.json');
 const filepath3 = getFixturePath('file1.yml');
 const filepath4 = getFixturePath('file2.yaml');
+const expected = readFile('expected-stylish.txt');
 
 test('difference between two JSONs', () => {
   expect(genDiff(filepath1, filepath2)).toBe(expected);
 });
 
 test('difference between two YAMLs', () => {
-  expect(genDiff(filepath1, filepath2)).toBe(expected);
-});
-
-test('difference between JSON and YAML', () => {
-  expect(genDiff(filepath1, filepath2)).toBe(expected);
   expect(genDiff(filepath3, filepath4)).toBe(expected);
 });
