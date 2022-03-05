426790d9d9c3a2525e9d941fd6389e70c9740e74
Max <ppmax@bk.ru>
2022-03-05 05:39:43 +0300

    Parser updated

--- src/parsers.js
+++ src/parsers.js
@@ -1,10 +1,13 @@
-import fs from 'fs';
+import { readFileSync } from 'fs';
+import { fileURLToPath } from 'url';
 import path from 'path';
 import yaml from 'js-yaml';
 
 const parseFile = (filepath) => {
-  const checkPath = path.isAbsolute(filepath) ? filepath : path.resolve(filepath);
-  const file = fs.readFileSync(checkPath, 'utf-8');
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+  const checkPath = path.isAbsolute(filepath) ? filepath : path.join(__dirname, '..', '__fixtures__', filepath);
+  const file = readFileSync(checkPath, 'utf-8');
   return path.extname(file) === '.json' ? JSON.parse(file) : yaml.load(file);
 };
 
