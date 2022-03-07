43c7436a0e58aaf8f6f5b3580d5a6b4ebe4b8d17
Max <ppmax@bk.ru>
2022-03-08 00:26:44 +0300

    Fixed formatter

--- src/formatters/format.js
+++ src/formatters/format.js
@@ -1,10 +1,13 @@
 import stylish from './stylish.js';
 import json from './json.js';
+import plain from './plain.js';
 
 const format = (file, formatName = 'stylish') => {
   switch (formatName) {
     case 'stylish':
       return stylish(file);
+    case 'plain':
+      return plain(file);
     default:
       return json(file);
   }
