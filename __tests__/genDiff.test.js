2e1780b43bfc71d88e39f9377167d145c274f72d
Max <ppmax@bk.ru>
2022-03-08 00:26:31 +0300

    Added new tests for plain

--- __tests__/genDiff.test.js
+++ __tests__/genDiff.test.js
@@ -15,12 +15,21 @@ const filepath1 = getFixturePath('file1.json');
 const filepath2 = getFixturePath('file2.json');
 const filepath3 = getFixturePath('file1.yml');
 const filepath4 = getFixturePath('file2.yaml');
-const expected = readFile('expected-stylish.txt');
+const expectedStylish = readFile('expected-stylish.txt');
+const expectedPlain = readFile('expected-plain.txt');
 
-test('difference between two JSONs', () => {
-  expect(genDiff(filepath1, filepath2)).toBe(expected);
+test('difference between two JSONs with stylish', () => {
+  expect(genDiff(filepath1, filepath2)).toBe(expectedStylish);
 });
 
-test('difference between two YAMLs', () => {
-  expect(genDiff(filepath3, filepath4)).toBe(expected);
+test('difference between two JSONs with plain', () => {
+  expect(genDiff(filepath1, filepath2, 'plain')).toBe(expectedPlain);
+});
+
+test('difference between two YAMLs with stylish', () => {
+  expect(genDiff(filepath3, filepath4)).toBe(expectedStylish);
+});
+
+test('difference between two YAMLs with plain', () => {
+  expect(genDiff(filepath3, filepath4, 'plain')).toBe(expectedPlain);
 });
