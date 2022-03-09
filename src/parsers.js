1d26a175cd47cbd21e243e396dec45f317a0ecba
Max <ppmax@bk.ru>
2022-03-09 23:02:03 +0300

    Refactoring #1

--- src/parsers.js
+++ src/parsers.js
@@ -1,24 +1,17 @@
-import { readFileSync } from 'fs';
-import { fileURLToPath } from 'url';
-import path from 'path';
 import yaml from 'js-yaml';
+import _ from 'lodash';
 
-const parseFile = (filepath) => {
-  const __filename = fileURLToPath(import.meta.url);
-  const __dirname = path.dirname(__filename);
-  const checkPath = path.isAbsolute(filepath) ? filepath : path.join(__dirname, '..', '__fixtures__', filepath);
-  const file = readFileSync(checkPath, 'utf-8');
-  const extension = path.extname(checkPath);
+const formats = {
+  json: JSON.parse,
+  yml: yaml.load,
+  yaml: yaml.load,
+};
 
-  switch (extension) {
-    case '.json':
-      return JSON.parse(file);
-    case '.yml':
-    case '.yaml':
-      return yaml.load(file);
-    default:
-      throw new Error('Unknown extension!');
+const parseFile = (file, extension) => {
+  if (!_.has(formats, extension)) {
+    throw new Error('Unknown extension!');
   }
+  return formats[extension](file);
 };
 
 export default parseFile;
