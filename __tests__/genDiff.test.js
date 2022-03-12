60614d1ae121d1b6ce8dbf4bca2e1a195c4d15be
Max <ppmax@bk.ru>
2022-03-12 07:00:06 +0300

    Refactoring tests #1

--- __tests__/genDiff.test.js
+++ __tests__/genDiff.test.js
@@ -1,56 +1,37 @@
-/* eslint-disable jest/no-commented-out-tests */
 import path from 'path';
 import { readFileSync } from 'fs';
 import { fileURLToPath } from 'url';
 import genDiff from '../src/index.js';
 
-const getFixturePath = (filename) => {
-  const __filename = fileURLToPath(import.meta.url);
-  const __dirname = path.dirname(__filename);
-  return path.join(__dirname, '..', '__fixtures__', filename);
-};
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-const readFile = (filename) => readFileSync(getFixturePath(filename), 'utf-8');
+const buildFullPath = (filepath) => (path.isAbsolute(filepath) ? filepath : path.join(__dirname, '..', '__fixtures__', filepath));
 
-const filepath1 = getFixturePath('file1.json');
-const filepath2 = getFixturePath('file2.json');
-const filepath3 = getFixturePath('file1.yml');
-const filepath4 = getFixturePath('file2.yaml');
-const wrongFileExtension = getFixturePath('file1.txt');
+const readFile = (filename) => readFileSync(buildFullPath(filename), 'utf-8');
+
 const expectedStylish = readFile('expected-stylish.txt');
 const expectedPlain = readFile('expected-plain.txt');
 const expectedJSON = readFile('expected-json.txt');
 
-test('difference between two JSONs in stylish', () => {
-  expect(genDiff(filepath1, filepath2)).toBe(expectedStylish);
+const extensions = ['json', 'yml', 'yaml'];
+
+test.each([extensions])('Compare two files', (formatName) => {
+  const firstFile = buildFullPath(`file1.${formatName}`);
+  const secondFile = buildFullPath(`file2.${formatName}`);
+  expect(genDiff(firstFile, secondFile)).toEqual(expectedStylish);
+  expect(genDiff(firstFile, secondFile, 'stylish')).toEqual(expectedStylish);
+  expect(genDiff(firstFile, secondFile, 'plain')).toEqual(expectedPlain);
+  expect(genDiff(firstFile, secondFile, 'json')).toEqual(expectedJSON);
 });
 
-test('difference between two JSONs in plain', () => {
-  expect(genDiff(filepath1, filepath2, 'plain')).toBe(expectedPlain);
-});
+test.each([extensions])('Wrong file extension or format name', (formatName) => {
+  const wrongExtension = buildFullPath('file1.txt');
+  const firstFile = buildFullPath(`file1.${formatName}`);
+  const secondFile = buildFullPath(`file2.${formatName}`);
 
-test('difference between two JSONs in json', () => {
-  expect(genDiff(filepath1, filepath2, 'json')).toBe(expectedJSON);
-});
-
-test('difference between two YAMLs in stylish', () => {
-  expect(genDiff(filepath3, filepath4)).toBe(expectedStylish);
-});
-
-test('difference between two YAMLs in plain', () => {
-  expect(genDiff(filepath3, filepath4, 'plain')).toBe(expectedPlain);
-});
-
-test('difference between two YAMLs in json', () => {
-  expect(genDiff(filepath3, filepath4, 'json')).toBe(expectedJSON);
-});
-
-test('wrong file extention', () => {
-  expect(() => genDiff(wrongFileExtension, filepath4, 'json'))
+  expect(() => genDiff(wrongExtension, secondFile, 'json'))
     .toThrowError('Unknown extension!');
-});
-
-test('wrong format', () => {
-  expect(() => genDiff(filepath1, filepath2, 'smlish'))
+  expect(() => genDiff(firstFile, secondFile, 'smlish'))
     .toThrowError('Unknown format!');
 });
