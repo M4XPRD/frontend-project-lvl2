31aa22812ef2987733dbe6689b23b6f9ea700c3f
Max <ppmax@bk.ru>
2022-03-16 00:50:41 +0300

    Final refactoring

--- src/index.js
+++ src/index.js
@@ -1,50 +1,10 @@
-/* eslint-disable max-len */
 import { readFileSync } from 'fs';
-import { fileURLToPath } from 'url';
 import path from 'path';
 import parseFile from './parsers.js';
 import objectDiff from './diff.js';
 import format from './formatters/format.js';
 
-const buildFullPath = (filepath) => {
-  const __filename = fileURLToPath(import.meta.url);
-  const __dirname = path.dirname(__filename);
-  return path.isAbsolute(filepath) ? filepath : path.join(__dirname, '..', '__fixtures__', filepath);
-};
-
-// const buildFullPath = (filepath) => path.resolve('__fixtures__', filepath);
-
-// !!!!!!!!!!!!!!!!!!! Комментарий №2 !!!!!!!!!!!!!!!!!!!!!!
-// Проблему с process.cwd() так и не получилось решить. Может дело вообще в операционке?
-// Я работаю из Windows в VSCode, может ли здесь быть проблема?
-// Сейчас оставлю первый вариант согласно статье: https://ru.hexlet.io/blog/posts/chto-takoe-__dirname-v-javascript
-
-// const buildFullPath = (filepath) => {
-//   const __filename = fileURLToPath(import.meta.url);
-//   const __dirname = path.dirname(__filename);
-//   return path.isAbsolute(filepath) ? filepath : path.join(__dirname, '..', '__fixtures__', filepath);
-// };
-
-// console.log(buildFullPath('file1.json')); // c:\Users\advma\Desktop\Folder\frontend-project-lvl2\__fixtures__\file1.json
-
-// const buildFullPath = (filepath) => path.resolve(process.cwd(), filepath);
-
-// console.log(buildFullPath('file1.json')); // c:\Users\advma\Desktop\Folder\frontend-project-lvl2\file1.json
-
-// !!!!!!!!!!!!!! Комментарий №1 для наставника: !!!!!!!!!!!!!!!!!!!!!
-// Добрый день, Александр!
-// Насчёт функции выше. Я понимаю, что нужно написать следующее:
-// const buildFullPath = (filepath) => path.resolve(process.cwd(), filepath);
-
-// В итоге он выдаёт правильный путь. Но у меня получается ошибка формата
-// node:fs:585 handleErrorFromBinding(ctx);
-// Error: ENOENT: no such file or directory, open '/mnt/c/Users/advma/Desktop/Folder/frontend-project-lvl2/file1.json'
-
-// В итоге я не могу запустить команду gendiff из корневой папки. Не знаю, как это решить без помощи __dirname
-// В строчках 17 и 21 видно, что не хватает директории, если использовать process.cwd(), т.к. я работаю из корня
-
-// Работает только такой вариант функции (он используется в начале файла):
-// const buildFullPath = (filepath) => path.resolve('__fixtures__', filepath);
+const buildFullPath = (filepath) => path.resolve(process.cwd(), filepath);
 
 const getExtension = (filepath) => path.extname(filepath).slice(1).toLowerCase();
 
